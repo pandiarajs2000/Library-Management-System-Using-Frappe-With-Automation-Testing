6ee8dd9234a24e6729774a11baf0204d2c7e0d1a
Pandiaraj <pandiarajs2000@gmail.com>
2025-11-04 22:54:11 +0530

    added the negative case function

--- tests/test_scripts/test_library_member.py
+++ tests/test_scripts/test_library_member.py
@@ -1,10 +1,13 @@
 from pages.login_page import LoginClass
-from tests.pages.library_member import LibraryMember
+from pages.library_member import LibraryMember
 from utils.excel_utils import read_data, write_data
 from utils.logger import logger
 import pytest
 import time
 import allure
+import logging
+
+logger = logging.getLogger(__name__)
 
 @allure.title("Test Valid Library Member")
 @allure.testcase("TC-001")
@@ -33,12 +36,13 @@ def test_valid_login(page, excel_sheet,base_url):
     status_cell = (2,7)
     member_expected_msg_cell = (2,8)
 
-    member_fullname_read_from_excel = read_data(excel_sheet_path, sheet_name, *fullname_cell)
-    member_email_read_from_excel = read_data(excel_sheet_path, sheet_name, *member_email_cell)
-    member_phone_read_from_excel = read_data(excel_sheet_path, sheet_name, *phone)
-    member_doj_read_from_excel = read_data(excel_sheet, sheet_name, *date_of_joining_cell)
-    status_cell_read_from_excel = read_data(excel_sheet, sheet_name, *status_cell)
-    member_expectedmsg_read_from_excel = read_data(excel_sheet, sheet_name, *member_expected_msg_cell)
+    member_fullname_read_from_excel = read_data(excel_sheet_path, sheet_name2, *fullname_cell)
+    member_email_read_from_excel = read_data(excel_sheet_path, sheet_name2, *member_email_cell)
+    member_phone_read_from_excel = read_data(excel_sheet_path, sheet_name2, *phone)
+    member_doj_read_from_excel = read_data(excel_sheet, sheet_name2, *date_of_joining_cell)
+    status_cell_read_from_excel = read_data(excel_sheet, sheet_name2, *status_cell)
+    print("Status:", status_cell_read_from_excel)
+    member_expectedmsg_read_from_excel = read_data(excel_sheet, sheet_name2, *member_expected_msg_cell)
 
 
     login_form = LoginClass(page)
@@ -53,13 +57,71 @@ def test_valid_login(page, excel_sheet,base_url):
         response = library_member_form.validate_library_member_screen(
             member_fullname_read_from_excel,member_email_read_from_excel,
             member_phone_read_from_excel,member_doj_read_from_excel,
-            status_cell_read_from_excel,member_expectedmsg_read_from_excel
+            status_cell_read_from_excel
             )
     try:
-        assert expectedmsg_read_from_excel in res, f"Expected message: '{expectedmsg_read_from_excel}', but got: '{res}'"
-        assert member_expectedmsg_read_from_excel in res, f"Expected message: '{member_expectedmsg_read_from_excel}', but got: '{response}'"
-        write_data(file_path=excel_sheet_path, sheet_name=sheet_name, row=2, column=9, data=res)
-    except Exception as e:
-        assert expectedmsg_read_from_excel in res, f"Expected message: '{expectedmsg_read_from_excel}', but got: '{res}'"
-        assert member_expectedmsg_read_from_excel in res, f"Expected message: '{member_expectedmsg_read_from_excel}', but got: '{response}'"
-        write_data(file_path=excel_sheet_path, sheet_name=sheet_name, row=2, column=9, data=res)
+        with allure.step("Validate Login and Member Save Messages"):
+            assert expectedmsg_read_from_excel in res, f"Expected Login message: '{expectedmsg_read_from_excel}', but got: '{res}'"
+            assert member_expectedmsg_read_from_excel in response, f"Expected Member message: '{member_expectedmsg_read_from_excel}', but got: '{response}'"
+        write_data(file_path=excel_sheet_path, sheet_name=sheet_name2, row=2, column=9, data=response)
+    except AssertionError as e:
+        write_data(file_path=excel_sheet_path, sheet_name=sheet_name2, row=2, column=9, data=response)
+        raise e
+    
+@allure.title("Test the Existing Member Email ID Should not be Allowed.")
+@allure.testcase("TC-002")
+@allure.description("Test the Existing Member Email ID Should not be Allowed.")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.story("Test the Negative scenario for the existing member email id should not allowed.")
+def test_valid_login(page, excel_sheet,base_url):
+    excel_sheet_path = excel_sheet
+    print("Excel Sheet Path", excel_sheet_path)
+    # Login Datas Fetch from Excel.
+    sheet_name = "Login"
+    email_cell = (2, 3) 
+    password_cell = (2, 4)
+    expected_msg_cell = (2,5)
+    email_read_from_excel = read_data(excel_sheet_path, sheet_name, *email_cell)
+    password_read_from_excel = read_data(excel_sheet, sheet_name, *password_cell)
+    expectedmsg_read_from_excel = read_data(excel_sheet, sheet_name, *expected_msg_cell)
+    print("Email Read","=", email_read_from_excel)
+    print("Password Read","=", password_read_from_excel)
+    # Library Member Form Data Fetch From Excel
+    sheet_name2 = "Members"
+    fullname_cell = (3,3)
+    member_email_cell = (3,4)
+    phone = (3,5)
+    date_of_joining_cell = (3,6)
+    status_cell = (3,7)
+    member_expected_msg_cell = (3,8)
+
+    member_fullname_read_from_excel = read_data(excel_sheet_path, sheet_name2, *fullname_cell)
+    member_email_read_from_excel = read_data(excel_sheet_path, sheet_name2, *member_email_cell)
+    member_phone_read_from_excel = read_data(excel_sheet_path, sheet_name2, *phone)
+    member_doj_read_from_excel = read_data(excel_sheet, sheet_name2, *date_of_joining_cell)
+    status_cell_read_from_excel = read_data(excel_sheet, sheet_name2, *status_cell)
+    print("Status:", status_cell_read_from_excel)
+    member_expectedmsg_read_from_excel = read_data(excel_sheet, sheet_name2, *member_expected_msg_cell)
+
+
+    login_form = LoginClass(page)
+    logger.info("Navigating to login page")
+    with allure.step("Verify and Load the Site URL"):
+        login_form.login_page_load(base_url)
+    with allure.step("Enter credentials and submit login form"):
+        res = login_form.login_screen_validate(email_read_from_excel, password_read_from_excel)
+    
+    library_member_form = LibraryMember(page)
+    with allure.step("Open a Library Member Screen"):
+        response = library_member_form.validate_library_member_screen(
+            member_fullname_read_from_excel,member_email_read_from_excel,
+            member_phone_read_from_excel,member_doj_read_from_excel,
+            status_cell_read_from_excel
+            )
+    try:
+        with allure.step("Validate Login and Existing Member Should not be allowed."):
+            assert expectedmsg_read_from_excel in res, f"Expected Login message: '{expectedmsg_read_from_excel}', but got: '{res}'"
+            assert member_expectedmsg_read_from_excel in response, f"Expected Member message: '{member_expectedmsg_read_from_excel}', but got: '{response}'"
+        write_data(file_path=excel_sheet_path, sheet_name=sheet_name2, row=2, column=9, data=response)
+    except AssertionError as e:
+        write_data(file_path=excel_sheet_path, sheet_name=sheet_name2, row=2, column=9, data=response)
