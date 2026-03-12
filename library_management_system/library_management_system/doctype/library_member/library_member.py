ea8039cff655cd9f230a8269a7c17bab5f3ecab0
pandiarajs2000@gmail.com <pandiarajs2000@gmail.com>
2026-03-12 23:07:49 +0530

    fix: email validations before create the library member

--- library_management_system/library_management_system/doctype/library_member/library_member.py
+++ library_management_system/library_management_system/doctype/library_member/library_member.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2025, Pandiaraj and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class LibraryMember(Document):
-	pass
+	def before_insert(self):
+		if frappe.db.exists('Library Member', {'email': self.email}):
+			frappe.throw('A library member with this email already exists.')
