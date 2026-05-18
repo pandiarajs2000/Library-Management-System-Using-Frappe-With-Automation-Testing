e9e7a13fbc2e7fc93516a765c54934cdabebc98d
pandiarajs2000@gmail.com <pandiarajs2000@gmail.com>
2026-05-18 07:33:58 +0530

    added the json_data_read for login page data

--- tests/utils/excel_utils.py
+++ tests/utils/excel_utils.py
@@ -1,32 +1,47 @@
-import openpyxl
-def read_data(file_path, sheet_name, row, column):
-    wb = openpyxl.load_workbook(file_path)
-    sheet = wb[sheet_name]
-    return sheet.cell(row=row, column=column).value
+# import openpyxl
+# def read_data(file_path, sheet_name, row, column):
+#     wb = openpyxl.load_workbook(file_path)
+#     sheet = wb[sheet_name]
+#     return sheet.cell(row=row, column=column).value
 
-def write_data(file_path, sheet_name, row, column, data):
-    # Load the existing workbook
-    workbook = openpyxl.load_workbook(file_path)
-    if sheet_name in workbook.sheetnames:
-        worksheet = workbook[sheet_name]
-    else:
-        raise ValueError(f"Sheet '{sheet_name}' not found in the workbook.")
-    worksheet.cell(row=row, column=column, value=data)
-    workbook.save(file_path)
+# def write_data(file_path, sheet_name, row, column, data):
+#     # Load the existing workbook
+#     workbook = openpyxl.load_workbook(file_path)
+#     if sheet_name in workbook.sheetnames:
+#         worksheet = workbook[sheet_name]
+#     else:
+#         raise ValueError(f"Sheet '{sheet_name}' not found in the workbook.")
+#     worksheet.cell(row=row, column=column, value=data)
+#     workbook.save(file_path)
 
-def read_data_from_excel(file_path, sheet_name):
-    workbook = openpyxl.load_workbook(file_path)
-    sheet = workbook[sheet_name]
+# def read_data_from_excel(file_path, sheet_name):
+#     workbook = openpyxl.load_workbook(file_path)
+#     sheet = workbook[sheet_name]
 
-    # fetch the first row as header
-    headers = [cell.value for cell in sheet[1]]
+#     # fetch the first row as header
+#     headers = [cell.value for cell in sheet[1]]
     
-    print(headers)
+#     print(headers)
 
-    # fetch the data
-    data_list = []
+#     # fetch the data
+#     data_list = []
 
-    for data in sheet.iter_rows(min_row=2, values_only=True):
-        row_dict = dict(zip(headers, data))
-        data_list.append(row_dict)
-    return data_list
+#     for data in sheet.iter_rows(min_row=2, values_only=True):
+#         row_dict = dict(zip(headers, data))
+#         data_list.append(row_dict)
+#     return data_list
+
+import json
+
+def load_test_data():
+    with open("test_data/test_library_member.json") as file:
+        data = json.load(file)
+    return data
+
+def get_login_data():
+    data = load_test_data()
+    return data["login"]
+
+def get_library_member_data():
+    data = load_test_data()
+    return data["library_member"]
