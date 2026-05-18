8a147fc3829064353eb0402f8dfd5d8fc26cf21e
pandiarajs2000@gmail.com <pandiarajs2000@gmail.com>
2026-05-18 07:32:48 +0530

    remove the excel_file_fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,9 @@
-import pytest
-import os
-import time
 from playwright.sync_api import sync_playwright
+import pytest
 
 
 BASE_URL = "http://127.0.0.1:8001/#login"
-video_path = "videos"
+VIDEO_PATH = "videos"
 
 @pytest.fixture(scope="session")
 def browser_instance():
@@ -14,18 +12,10 @@ def browser_instance():
         yield browser
         browser.close()
 
-
-# @pytest.fixture(scope="session")
-# def browser():
-#     with sync_playwright() as p:
-#         browser = p.chromium.launch(headless=False)
-#         yield browser
-
-
 @pytest.fixture
 def page(browser_instance):
     context = browser_instance.new_context(
-        record_video_dir=video_path,
+        record_video_dir=VIDEO_PATH,
         record_video_size={"width":1280, "height":720}
     )
     page = context.new_page()
@@ -33,13 +23,13 @@ def page(browser_instance):
     yield page
     context.close()
 
-
-@pytest.fixture
-def excel_sheet():
-    excel_path = "/home/pandiaraj/library_management/apps/library_management_system/tests/test_data_sheet.xlsx"
-    return excel_path
-
-
 @pytest.fixture(scope="session")
 def base_url():
     return BASE_URL
+
+@pytest.fixture
+def read_test_data():
+    import json
+    with open("test_data.json", "r") as file:
+        data = json.load(file)
+    return data
