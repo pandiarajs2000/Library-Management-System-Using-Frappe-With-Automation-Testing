136e360aa43c4a76f6e42723bbb770e7fbb001a1
pandiarajs2000@gmail.com <pandiarajs2000@gmail.com>
2026-05-20 23:35:02 +0530

    fix:flaky test url change issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,27 +5,36 @@ import pytest
 BASE_URL = "http://127.0.0.1:8001/#login"
 VIDEO_PATH = "videos"
 
+# @pytest.fixture(scope="session")
+# def browser_instance():
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch(headless=False, slow_mo=2000)
+#         yield browser
+#         browser.close()
+
+# @pytest.fixture(scope="session")
+# def page(browser_instance):
+#     context = browser_instance.new_context(
+#         record_video_dir=VIDEO_PATH,
+#         record_video_size={"width":1280, "height":720}
+#     )
+#     page = context.new_page()
+#     page.set_default_timeout(30000)
+#     yield page
+#     context.close()
+
+# @pytest.fixture(scope="session")
+# def base_url():
+#     return BASE_URL
+
 @pytest.fixture(scope="session")
-def browser_instance():
+def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=2000)
-        yield browser
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("http://127.0.0.1:8001/#login")
+        yield page
         browser.close()
-
-@pytest.fixture
-def page(browser_instance):
-    context = browser_instance.new_context(
-        record_video_dir=VIDEO_PATH,
-        record_video_size={"width":1280, "height":720}
-    )
-    page = context.new_page()
-    page.set_default_timeout(30000)
-    yield page
-    context.close()
-
-@pytest.fixture(scope="session")
-def base_url():
-    return BASE_URL
 
 @pytest.fixture
 def read_test_data():
